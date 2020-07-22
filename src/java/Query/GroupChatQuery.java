f64682f60fdb82a91fe065a81ee4f03712aa270f
arkan481 <arkanharyo@gmail.com>
2020-07-22 08:29:04 +0700

    bug fix chat order

--- src/java/Query/GroupChatQuery.java
+++ src/java/Query/GroupChatQuery.java
@@ -13,7 +13,7 @@ public class GroupChatQuery {
     
     public final String GET = "SELECT message,date,users_tb.username,sender_id "
             + "FROM users_tb,group_chat_tb WHERE group_id = ? "
-            + "AND users_tb.id = sender_id";
+            + "AND users_tb.id = sender_id ORDER BY group_chat_tb.id";
     public final String CREATE = "INSERT INTO group_chat_tb"
             + "(group_id,message,sender_id) VALUES (?,?,?)";
     
